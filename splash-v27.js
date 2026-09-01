aa72c2906c7bb255b8561c5a072ccbb7db631056
wichyy1-design <wichyy_1@hotmail.com>
2026-09-01 12:28:21 -0300

    v27.3: alargar apertura estilo cinematografico

--- splash-v27.js
+++ splash-v27.js
@@ -19,9 +19,10 @@
     splash.classList.add('hide');
     document.documentElement.classList.remove('jdSplashActive');
     document.documentElement.classList.remove('jdBootSplash');
-    setTimeout(()=>splash.remove(),420);
+    setTimeout(()=>splash.remove(),520);
   };
 
   const reduced=window.matchMedia?.('(prefers-reduced-motion: reduce)')?.matches;
-  setTimeout(close,reduced?900:2150);
+  // Ritmo más cinematográfico: entrada, gesto/destello, marca y fundido.
+  setTimeout(close,reduced?1100:2950);
 })();
